4cd6e2092c0ba6a5076ad7f2572e54087a1eed5d
renugadevi26 <131246583+renugadevi26@users.noreply.github.com>
2025-06-30 11:02:29 +0530

    Add files via upload

---
+++ brain_tumor_resvit_seg.py
@@ -0,0 +1,439 @@
+import os
+import cv2
+import glob
+import PIL
+import shutil
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from skimage import data
+from skimage.util import montage 
+import skimage.transform as skTrans
+from skimage.transform import rotate
+from skimage.transform import resize
+from PIL import Image, ImageOps  
+
+
+# neural imaging
+import nilearn as nl
+import nibabel as nib
+import nilearn.plotting as nlplt
+
+
+
+# ml libs
+import keras
+import keras.backend as K
+from keras.callbacks import CSVLogger
+import tensorflow as tf
+from tensorflow.keras.utils import plot_model
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from tensorflow.keras.models import *
+from tensorflow.keras.layers import *
+from tensorflow.keras.optimizers import *
+from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping, TensorBoard
+from tensorflow.keras.layers.experimental import preprocessing
+
+import tensorflow as tf
+# Make numpy printouts easier to read.
+np.set_printoptions(precision=3, suppress=True)
+
+# DEFINE seg-areas  
+SEGMENT_CLASSES = {
+    0 : 'NOT tumor',
+    1 : 'NECROTIC/CORE', # or NON-ENHANCING tumor CORE
+    2 : 'EDEMA',
+    3 : 'ENHANCING' # original 4 -> converted into 3 later
+}
+
+# there are 155 slices per volume
+# to start at 5 and use 145 slices means we will skip the first 5 and last 5 
+VOLUME_SLICES = 100 
+VOLUME_START_AT = 22 # first slice of volume that we will include
+
+
+TRAIN_DATASET_PATH = "/nfsshare/knr/knrtrain/renu/Brats2020/BraTS2020_TrainingData/MICCAI_BraTS2020_TrainingData/"
+VALIDATION_DATASET_PATH = "/nfsshare/knr/knrtrain/renu/Brats2020/BraTS2020_ValidationData/MICCAI_BraTS2020_ValidationData/"
+
+test_image_flair=nib.load(TRAIN_DATASET_PATH + 'BraTS20_Training_001/BraTS20_Training_001_flair.nii').get_fdata()
+test_image_t1=nib.load(TRAIN_DATASET_PATH + 'BraTS20_Training_001/BraTS20_Training_001_t1.nii').get_fdata()
+test_image_t1ce=nib.load(TRAIN_DATASET_PATH + 'BraTS20_Training_001/BraTS20_Training_001_t1ce.nii').get_fdata()
+test_image_t2=nib.load(TRAIN_DATASET_PATH + 'BraTS20_Training_001/BraTS20_Training_001_t2.nii').get_fdata()
+test_mask=nib.load(TRAIN_DATASET_PATH + 'BraTS20_Training_001/BraTS20_Training_001_seg.nii').get_fdata()
+
+
+fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(1,5, figsize = (20, 10))
+slice_w = 25
+ax1.imshow(test_image_flair[:,:,test_image_flair.shape[0]//2-slice_w], cmap = 'gray')
+ax1.set_title('Image flair')
+ax2.imshow(test_image_t1[:,:,test_image_t1.shape[0]//2-slice_w], cmap = 'gray')
+ax2.set_title('Image t1')
+ax3.imshow(test_image_t1ce[:,:,test_image_t1ce.shape[0]//2-slice_w], cmap = 'gray')
+ax3.set_title('Image t1ce')
+ax4.imshow(test_image_t2[:,:,test_image_t2.shape[0]//2-slice_w], cmap = 'gray')
+ax4.set_title('Image t2')
+ax5.imshow(test_mask[:,:,test_mask.shape[0]//2-slice_w])
+ax5.set_title('Mask')
+
+# dice loss as defined above for 4 classes
+def dice_coef(y_true, y_pred, smooth=1.0):
+    class_num = 4
+    for i in range(class_num):
+        y_true_f = K.flatten(y_true[:,:,:,i])
+        y_pred_f = K.flatten(y_pred[:,:,:,i])
+        intersection = K.sum(y_true_f * y_pred_f)
+        loss = ((2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth))
+   #     K.print_tensor(loss, message='loss value for class {} : '.format(SEGMENT_CLASSES[i]))
+        if i == 0:
+            total_loss = loss
+        else:
+            total_loss = total_loss + loss
+    total_loss = total_loss / class_num
+#    K.print_tensor(total_loss, message=' total dice coef: ')
+    return total_loss
+
+
+ 
+# define per class evaluation of dice coef
+# inspired by https://github.com/keras-team/keras/issues/9395
+def dice_coef_necrotic(y_true, y_pred, epsilon=1e-6):
+    intersection = K.sum(K.abs(y_true[:,:,:,1] * y_pred[:,:,:,1]))
+    return (2. * intersection) / (K.sum(K.square(y_true[:,:,:,1])) + K.sum(K.square(y_pred[:,:,:,1])) + epsilon)
+
+def dice_coef_edema(y_true, y_pred, epsilon=1e-6):
+    intersection = K.sum(K.abs(y_true[:,:,:,2] * y_pred[:,:,:,2]))
+    return (2. * intersection) / (K.sum(K.square(y_true[:,:,:,2])) + K.sum(K.square(y_pred[:,:,:,2])) + epsilon)
+
+def dice_coef_enhancing(y_true, y_pred, epsilon=1e-6):
+    intersection = K.sum(K.abs(y_true[:,:,:,3] * y_pred[:,:,:,3]))
+    return (2. * intersection) / (K.sum(K.square(y_true[:,:,:,3])) + K.sum(K.square(y_pred[:,:,:,3])) + epsilon)
+
+
+
+# Computing Precision 
+def precision(y_true, y_pred):
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + K.epsilon())
+        return precision
+
+    
+# Computing Sensitivity      
+def sensitivity(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    return true_positives / (possible_positives + K.epsilon())
+
+
+# Computing Specificity
+def specificity(y_true, y_pred):
+    true_negatives = K.sum(K.round(K.clip((1-y_true) * (1-y_pred), 0, 1)))
+    possible_negatives = K.sum(K.round(K.clip(1-y_true, 0, 1)))
+    return true_negatives / (possible_negatives + K.epsilon())
+    
+    
+IMG_SIZE=256
+
+
+
+# lists of directories with studies
+train_and_val_directories = [f.path for f in os.scandir(TRAIN_DATASET_PATH) if f.is_dir()]
+
+# file BraTS20_Training_355 has ill formatted name for for seg.nii file
+train_and_val_directories.remove(TRAIN_DATASET_PATH+'BraTS20_Training_355')
+
+
+def pathListIntoIds(dirList):
+    x = []
+    for i in range(0,len(dirList)):
+        x.append(dirList[i][dirList[i].rfind('/')+1:])
+    return x
+
+train_and_test_ids = pathListIntoIds(train_and_val_directories); 
+
+    
+train_test_ids, val_ids = train_test_split(train_and_test_ids,test_size=0.2) 
+train_ids, test_ids = train_test_split(train_test_ids,test_size=0.15) 
+
+
+class DataGenerator(keras.utils.Sequence):
+    'Generates data for Keras'
+    def __init__(self, list_IDs, dim=(IMG_SIZE,IMG_SIZE), batch_size = 1, n_channels = 3, shuffle=True):
+        'Initialization'
+        self.dim = dim
+        self.batch_size = batch_size
+        self.list_IDs = list_IDs
+        self.n_channels = n_channels
+        self.shuffle = shuffle
+        self.on_epoch_end()
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.floor(len(self.list_IDs) / self.batch_size))
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        # Find list of IDs
+        Batch_ids = [self.list_IDs[k] for k in indexes]
+
+        # Generate data
+        X, y = self.__data_generation(Batch_ids)
+
+        return X, y
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(len(self.list_IDs))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, Batch_ids):
+        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
+        # Initialization
+        X = np.zeros((self.batch_size*VOLUME_SLICES, *self.dim, self.n_channels))
+        y = np.zeros((self.batch_size*VOLUME_SLICES, 240, 240))
+        Y = np.zeros((self.batch_size*VOLUME_SLICES, *self.dim, 4))
+
+        
+        # Generate data
+        for c, i in enumerate(Batch_ids):
+            case_path = os.path.join(TRAIN_DATASET_PATH, i)
+
+            data_path = os.path.join(case_path, f'{i}_flair.nii');
+            flair = nib.load(data_path).get_fdata()    
+
+            data_path = os.path.join(case_path, f'{i}_t1ce.nii');
+            ce = nib.load(data_path).get_fdata()
+            
+            data_path = os.path.join(case_path, f'{i}_seg.nii');
+            seg = nib.load(data_path).get_fdata()
+        
+            for j in range(VOLUME_SLICES):
+                 X[j +VOLUME_SLICES*c,:,:,0] = cv2.resize(flair[:,:,j+VOLUME_START_AT], (IMG_SIZE, IMG_SIZE));
+                 X[j +VOLUME_SLICES*c,:,:,1] = cv2.resize(ce[:,:,j+VOLUME_START_AT], (IMG_SIZE, IMG_SIZE));
+
+                 y[j +VOLUME_SLICES*c] = seg[:,:,j+VOLUME_START_AT];
+                    
+        # Generate masks
+        y[y==4] = 3;
+        mask = tf.one_hot(y, 4);
+        Y = tf.image.resize(mask, (IMG_SIZE, IMG_SIZE));
+        return X/np.max(X), Y
+        
+training_generator = DataGenerator(train_ids)
+valid_generator = DataGenerator(val_ids)
+test_generator = DataGenerator(test_ids)
+
+
+
+# show number of data for each dir 
+def showDataLayout():
+    plt.bar(["Train","Valid","Test"],
+    [len(train_ids), len(val_ids), len(test_ids)], align='center',color=[ 'green','red', 'blue'])
+    plt.legend()
+
+    plt.ylabel('Number of images')
+    plt.title('Data distribution')
+
+    plt.show()
+    
+showDataLayout()
+
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import backend as K
+from tensorflow.keras.layers import Input, Conv2D, BatchNormalization, Activation, MaxPool2D, Conv2DTranspose, Concatenate
+from tensorflow.keras.models import Model
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger, ReduceLROnPlateau, EarlyStopping
+from keras_vit.vit import ViT_B16
+import keras_vit.layers as layers
+layers.tf = tf
+print('model prepration')
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# Define functions for model building
+def create_resnet50_encoder(input_shape):
+    base_model = ResNet50(weights=None, include_top=False, input_shape=input_shape)
+    inp = Input(shape=input_shape)
+    output_layer = base_model(inp)
+    layer_to_keep = base_model.layers[-36].output
+
+
+def conv_block(inputs, num_filters):
+    x = Conv2D(num_filters, 3, padding="same")(inputs)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+
+    x = Conv2D(num_filters, 3, padding="same")(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+
+    return x
+
+def encoder_block(inputs, num_filters):
+    x = conv_block(inputs, num_filters)
+    p = MaxPool2D((2, 2))(x)
+    return x, p
+
+def decoder_block(inputs, skip_features, num_filters):
+    x = Conv2DTranspose(num_filters, 2, strides=2, padding="same")(inputs)
+    x = Concatenate()([x, skip_features])
+    x = conv_block(x, num_filters)
+    return x
+
+def build_unet(input_shape):
+    # Load ResNet-50 with pre-trained weights
+    base_model = ResNet50(weights=None, include_top=False, input_shape=input_shape)
+    layer_to_keep = base_model.layers[-36].output
+
+    vit = ViT_B16(image_size=256,pre_trained=False,include_mlp_head=256)
+
+    # Create a model that includes both the ViT and ResNet models
+    combined_input = base_model.input
+    vit_output = vit.layers[-2]
+    vit_output=vit_output(combined_input)
+    # Global Average Pooling
+    
+    vit_output=tf.keras.layers.Reshape((16,16,3))(vit_output)
+    
+    # U-Net architecture
+    inputs = base_model.input  # Use the input from the loaded ResNet-50 model
+    s1, p1 = encoder_block(inputs, 64)
+    s2, p2 = encoder_block(p1, 128)
+    s3, p3 = encoder_block(p2, 256)
+    s4, p4 = encoder_block(p3, 512)
+
+    b1 = conv_block(p4, 1024)
+    b1 = Concatenate()([b1, layer_to_keep,vit_output])
+
+    d1 = decoder_block(b1, s4, 512)
+    d2 = decoder_block(d1, s3, 256)
+    d3 = decoder_block(d2, s2, 128)
+    d4 = decoder_block(d3, s1, 64)
+
+    outputs = Conv2D(4, 1, padding="same", activation="softmax")(d4)
+
+    model = Model(inputs, outputs, name="UNET")
+    return model
+
+# Example usage
+unet_model = build_unet((256, 256, 3))
+
+unet_model.summary()
+
+# Load and preprocess your dataset
+# ... (assuming xtr, ytr, xte, yte are available)
+
+# Assuming xtr, ytr, xte, yte are your training and validation data
+lr = 1e-6
+smooth = 1e-15
+# Assuming your model takes float32 inputs
+
+
+
+
+# Compile your U-Net model
+
+# dice loss as defined above for 4 classes
+def dice_coef(y_true, y_pred, smooth=1.0):
+    class_num = 4
+    for i in range(class_num):
+        y_true_f = K.flatten(y_true[:,:,:,i])
+        y_pred_f = K.flatten(y_pred[:,:,:,i])
+        intersection = K.sum(y_true_f * y_pred_f)
+        loss = ((2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth))
+   #     K.print_tensor(loss, message='loss value for class {} : '.format(SEGMENT_CLASSES[i]))
+        if i == 0:
+            total_loss = loss
+        else:
+            total_loss = total_loss + loss
+    total_loss = total_loss / class_num
+#    K.print_tensor(total_loss, message=' total dice coef: ')
+    return total_loss
+
+
+ 
+# define per class evaluation of dice coef
+# inspired by https://github.com/keras-team/keras/issues/9395
+def dice_coef_necrotic(y_true, y_pred, epsilon=1e-6):
+    intersection = K.sum(K.abs(y_true[:,:,:,1] * y_pred[:,:,:,1]))
+    return (2. * intersection) / (K.sum(K.square(y_true[:,:,:,1])) + K.sum(K.square(y_pred[:,:,:,1])) + epsilon)
+
+def dice_coef_edema(y_true, y_pred, epsilon=1e-6):
+    intersection = K.sum(K.abs(y_true[:,:,:,2] * y_pred[:,:,:,2]))
+    return (2. * intersection) / (K.sum(K.square(y_true[:,:,:,2])) + K.sum(K.square(y_pred[:,:,:,2])) + epsilon)
+
+def dice_coef_enhancing(y_true, y_pred, epsilon=1e-6):
+    intersection = K.sum(K.abs(y_true[:,:,:,3] * y_pred[:,:,:,3]))
+    return (2. * intersection) / (K.sum(K.square(y_true[:,:,:,3])) + K.sum(K.square(y_pred[:,:,:,3])) + epsilon)
+
+
+
+# Computing Precision 
+def precision(y_true, y_pred):
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + K.epsilon())
+        return precision
+
+    
+# Computing Sensitivity      
+def sensitivity(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    return true_positives / (possible_positives + K.epsilon())
+
+
+# Computing Specificity
+def specificity(y_true, y_pred):
+    true_negatives = K.sum(K.round(K.clip((1-y_true) * (1-y_pred), 0, 1)))
+    possible_negatives = K.sum(K.round(K.clip(1-y_true, 0, 1)))
+    return true_negatives / (possible_negatives + K.epsilon())
+    
+    
+lr = 1e-4
+unet_model.compile(optimizer=Adam(lr), loss='categorical_crossentropy', metrics=['accuracy',dice_coef,precision,sensitivity,specificity])
+# Define callbacks
+
+callbacks = [
+    ModelCheckpoint("/nfsshare/knr/knrtrain/renu/resnet_vit/brain_tumor_vit_res_weights.h5", verbose=1, save_best_only=True, monitor='val_loss', mode='min', save_weights_only=True),
+    ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, min_lr=1e-7, verbose=1),
+    CSVLogger("/nfsshare/knr/knrtrain/renu/resnet_vit/brain_tumor_vit_res_csv.log"),
+]
+unet_model.compile(loss="categorical_crossentropy", optimizer=keras.optimizers.Adam(learning_rate=0.001), metrics = ['accuracy',tf.keras.metrics.MeanIoU(num_classes=4), dice_coef, precision, sensitivity, specificity, dice_coef_necrotic, dice_coef_edema ,dice_coef_enhancing] )
+
+
+
+unet_model.load_weights("/nfsshare/knr/knrtrain/renu/resnet_vit/brain_tumor_vit_res_weights.h5")
+
+
+
+unet_model.fit(training_generator,epochs=50,validation_data=valid_generator,callbacks=callbacks)
+
+
+
+
+
+
+
+
+
+
+
+model.compile(loss="categorical_crossentropy", optimizer=keras.optimizers.Adam(learning_rate=0.001), metrics = ['accuracy',tf.keras.metrics.MeanIoU(num_classes=4), dice_coef, precision, sensitivity, specificity, dice_coef_necrotic, dice_coef_edema, dice_coef_enhancing] )
+# Evaluate the model on the test data using `evaluate`
+print("Evaluate on test data")
+results = unet_model.evaluate(test_generator, batch_size=100, callbacks= callbacks)
+print("test loss, test acc:", results)
+
---
+++ vit_dense.py
@@ -0,0 +1,155 @@
+import cv2
+import os
+import tensorflow as tf
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import backend as K
+from tensorflow.keras.layers import Input, Conv2D, BatchNormalization, Activation, MaxPool2D, Conv2DTranspose, Concatenate
+from tensorflow.keras.models import Model
+from tensorflow.keras.applications import DenseNet121
+from tensorflow.keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger, ReduceLROnPlateau, EarlyStopping
+from keras_vit.vit import ViT_B16
+import keras_vit.layers as layers
+layers.tf = tf
+
+
+cl=os.listdir("/nfsshare/knr/knrtrain/renu/figshare/figshare-dataset/")
+lb=[]
+imgs=[]
+for i in cl:
+    mn=f'/nfsshare/knr/knrtrain/renu/figshare/figshare-dataset/{i}'
+    idx=cl.index(i)
+    for j in os.listdir(mn):
+        img=cv2.imread(os.path.join(mn,j))
+        r=cv2.resize(img,(256,256))
+        imgs.append(r)
+        lb.append(idx)
+import numpy as np
+imgs=np.array(imgs)
+lb=np.array(lb)
+
+
+#one-hot
+lb=tf.keras.utils.to_categorical(lb,num_classes=3)
+
+
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Flatten
+
+from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger, ReduceLROnPlateau, EarlyStopping
+# Define input shape
+input_shape = (256, 256, 3)
+
+# Load the InceptionV3 model with pretrained weights set to None
+
+
+def conv_block(inputs, num_filters):
+    x = Conv2D(num_filters, 3, padding="same")(inputs)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+
+    x = Conv2D(num_filters, 3, padding="same")(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+
+    return x
+
+def encoder_block(inputs, num_filters):
+    x = conv_block(inputs, num_filters)
+    p = MaxPool2D((2, 2))(x)
+    return x, p
+def build_unet(input_shape):
+    # Load ResNet-50 with pre-trained weights
+    base_model = DenseNet121(weights=None, include_top=False, input_shape=input_shape)
+    layer_to_keep = base_model.layers[-116].output
+
+    vit = ViT_B16(image_size=256,pre_trained=False,include_mlp_head=256)
+
+    # Create a model that includes both the ViT and ResNet models
+    combined_input = base_model.input
+    vit_output = vit.layers[-2]
+    vit_output=vit_output(combined_input)
+    # Global Average Pooling
+    
+    vit_output=tf.keras.layers.Reshape((16,16,3))(vit_output)
+    
+    # U-Net architecture
+    inputs = base_model.input  # Use the input from the loaded ResNet-50 model
+    s1, p1 = encoder_block(inputs, 64)
+    s2, p2 = encoder_block(p1, 128)
+    s3, p3 = encoder_block(p2, 256)
+    s4, p4 = encoder_block(p3, 512)
+
+    b1 = conv_block(p4, 1024)
+    b1 = Concatenate()([b1, layer_to_keep,vit_output])
+    
+    d1= Flatten()(b1)
+    d2= Dense(128,activation='relu')(d1)
+    outputs= Dense(3,activation='softmax')(d2)
+    
+    
+
+    model = Model(inputs, outputs, name="UNET")
+    return model
+
+
+unet_model = build_unet((256, 256, 3))
+
+
+# Computing Precision 
+def precision(y_true, y_pred):
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + K.epsilon())
+        return precision
+
+    
+# Computing Sensitivity      
+def sensitivity(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    return true_positives / (possible_positives + K.epsilon())
+
+
+# Computing Specificity
+def specificity(y_true, y_pred):
+    true_negatives = K.sum(K.round(K.clip((1-y_true) * (1-y_pred), 0, 1)))
+    possible_negatives = K.sum(K.round(K.clip(1-y_true, 0, 1)))
+    return true_negatives / (possible_negatives + K.epsilon())
+    
+    
+def f1_score(y_true, y_pred):
+    y_true = tf.cast(y_true, tf.float32)
+    y_pred = tf.cast(tf.math.round(y_pred), tf.float32)
+
+    # True Positives
+    tp = tf.reduce_sum(y_true * y_pred, axis=0)
+    # False Positives
+    fp = tf.reduce_sum((1 - y_true) * y_pred, axis=0)
+    # False Negatives
+    fn = tf.reduce_sum(y_true * (1 - y_pred), axis=0)
+
+    # Calculate precision and recall
+    precision = tp / (tp + fp + tf.keras.backend.epsilon())
+    recall = tp / (tp + fn + tf.keras.backend.epsilon())
+
+    # Calculate F1 score
+    f1 = 2 * (precision * recall) / (precision + recall + tf.keras.backend.epsilon())
+
+    # Average F1 score across all classes
+    f1 = tf.reduce_mean(f1)
+
+    return f1
+
+# Compile the model
+unet_model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy',precision,sensitivity,specificity,tf.keras.metrics.Recall(),f1_score])
+callbacks = [
+    ModelCheckpoint('/nfsshare/knr/knrtrain/renu/figshare/vit_dense/vit_dense.weights10.h5', verbose=1, save_best_only=True, monitor='val_loss', mode='min', save_weights_only=True),
+    ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, min_lr=1e-7, verbose=1),
+    CSVLogger('/nfsshare/knr/knrtrain/renu/figshare/vit_dense/vit_dense10.csv')
+]
+unet_model.fit(imgs,lb,epochs=10,validation_split=0.2,batch_size=32,callbacks=callbacks)
+unet_model.save('/nfsshare/knr/knrtrain/renu/figshare/vit_dense/vit_dense_complete_model.h5')
